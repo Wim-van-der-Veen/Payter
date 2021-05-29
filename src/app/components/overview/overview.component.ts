0e76898837f204233ca87a0631f829c41d994fd2
Wim van der Veen <wim.veen@breingolf.eu>
2021-05-29 17:31:39 +0200

    added loading indicator to overview
    also includes error-catcher for possibly listItems error

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { BehaviorSubject, Subject, Observable } from 'rxjs';
+import { BehaviorSubject, Subject, Observable, of } from 'rxjs';
 
 import { DataItem } from 'app/model';
 import { DataService } from 'app/services/data';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-overview',
@@ -13,6 +13,7 @@ import { map, switchMap, tap } from 'rxjs/operators';
 })
 export class OverviewComponent implements OnInit {
 
+  loading = false; // 20210529 WV whether or not loading indicator is visible
   items$: Observable<DataItem[]>;
 
   constructor(
@@ -22,9 +23,19 @@ export class OverviewComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.loading = true; // 20210529 WV show loading indicator
     this.items$ = this.route.queryParams.pipe(
       // TODO implement filter here
-      switchMap(() => this.dataService.listItems())
+      switchMap(() => this.dataService.listItems()
+        .pipe( // 20210529 WV tapped completion of the listItems
+          tap(() => this.loading = false), // clear the loading indicator
+        )
+      ),
+      catchError(err => { // 20210529 WV catch possible error in listItems
+        console.error('myError', err); // clear the loading indicator
+        setTimeout(() => this.loading = false, 0); // necessary to wrap in timeout() to avoid 'Expression changed after checked' error
+        return []; // return empty stream
+      }),
     );
   }
 
