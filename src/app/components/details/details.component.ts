a9768d633cfca38a4550fdf7a0594b13e3e38101
Wim van der Veen <wim.veen@breingolf.eu>
2021-05-29 17:32:08 +0200

    added loading indicator to details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -12,6 +12,7 @@ import { DataService } from 'app/services/data';
 })
 export class DetailsComponent implements OnInit {
 
+  loading = false; // 20210529 WV whether or not loading indicator is visible
   $item: Subject<DataItem> = new BehaviorSubject<DataItem>(null);
 
   constructor(
@@ -21,13 +22,16 @@ export class DetailsComponent implements OnInit {
     ) {}
 
   ngOnInit() {
+    this.loading = true; // 20210529 WV show loading indicator
     this.route.params.subscribe(params => {
       const id = Number(params.id);
       this.dataService.getItem(id).subscribe(item => {
         this.$item.next(item);
+        this.loading = false; // 20210529 WV clear loading indicator
       }, err => {
         this.$item.next(null);
         console.log(err);
+        this.loading = false; // 20210529 WV clear loading indicator
       });
     });
   }
